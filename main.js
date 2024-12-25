c7642f795e231d3d810f7bbad4130cd6ec9b457c
Cory Spitzer <3945059+CorySpitzer@users.noreply.github.com>
2024-12-25 12:06:26 -0500

    Make many pixels

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const square = document.createElement('div');
-square.classList.add('pixel');
-const divContent = document.createTextNode('');
-square.appendChild(divContent);
-const parentDiv = document.getElementById('board');
-parentDiv.appendChild(square);
+for (let i = 0; i < 16; i++) {
+
+    const square = document.createElement('div');
+    square.classList.add('pixel');
+    const divContent = document.createTextNode('');
+    square.appendChild(divContent);
+    const parentDiv = document.getElementById('board');
+    parentDiv.appendChild(square);
+}
